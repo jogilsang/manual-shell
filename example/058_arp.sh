1fe8f518a0e7a8a5b7e3952d03b297abde3ce32a
jogilsang <jogilsang@gmail.com>
2020-11-09 17:54:07 +0000

    added : 059_hostip.sh about host

--- example/058_arp.sh
+++ example/058_arp.sh
@@ -4,6 +4,7 @@ ipaddr="172.31.0.1"
 
 # macaddr=$(arp -an | awk "/\($ipaddr \)/ {print \$4 }")
 macaddr=$(arp -an | awk '/\(172.31.0.1\)/ {print $4}') 
+# arp -an | awk '/172.31.0.1/ {print $4}'
 
 if [ -n "$macaddr " ];then
 	echo "$ipaddr -> $macaddr"
